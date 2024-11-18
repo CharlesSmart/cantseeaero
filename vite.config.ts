79bf97ed9fcf3c4af45c401a96aeeefbd23a10e8
Charles <charles.macfie.smart@gmail.com>
2024-11-18 15:05:50 +1300

    Attempt to resolve top-level await issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 export default defineConfig({
-  plugins: [react({
-    // babel: {
-    //   plugins: ["@onlook/babel-plugin-react"]
-    // }
-  })],
+  plugins: [react()],
+  build: {
+    target: 'esnext', // Allows for top-level await
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
