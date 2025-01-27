a7e5ba5a3fde87f6150cf0592c5acb166c09e960
Charles <charles.macfie.smart@gmail.com>
2025-01-28 10:37:42 +1300

    Removing the landing page subfolders etc

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
-  base: "/app/",
   build: {
     target: 'esnext', // Allows for top-level await
   },
