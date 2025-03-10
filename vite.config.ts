a0401c0d95d41817aff682946c8f08688e3c4af5
Charles <charles.macfie.smart@gmail.com>
2025-03-11 08:29:44 +1300

    Temp commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,26 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
 export default defineConfig({
   plugins: [
     react(),
-    nodePolyfills()
+    nodePolyfills({
+      protocolImports: true,
+    })
   ],
   build: {
     target: 'esnext', // Allows for top-level await
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
-      "readable-stream": "vite-compatible-readable-stream"
     }
   },
   define: {
-    global: {},
+    global: {}
   }
 });
