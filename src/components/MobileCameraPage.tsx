dfae60a0e146315a7e06e77f7ebf8b37de2f26f9
Charles <charles.macfie.smart@gmail.com>
2025-03-10 21:42:51 +1300

    polyfills

--- src/components/MobileCameraPage.tsx
+++ src/components/MobileCameraPage.tsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react';
 import global from 'global'
 import * as process from "process";
+import { Buffer } from 'buffer';
 import { io, Socket } from 'socket.io-client';
-import SimplePeer from 'simple-peer';
+import * as SimplePeer from 'simple-peer';
 import { useSearchParams } from 'react-router-dom';
+
+(window as any).global = window;
+(window as any).process = process;
+(window as any).Buffer = Buffer;
 
 global.process = process;
 
