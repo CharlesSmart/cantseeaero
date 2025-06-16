3b65708d9efcf63f4a1c9069172d0f132e8991d6
Charles <charles.macfie.smart@gmail.com>
2025-06-16 16:36:55 +1200

    max height on camera preview

--- src/components/CameraPreviewCard.tsx
+++ src/components/CameraPreviewCard.tsx
@@ -10,12 +10,12 @@ interface CameraPreviewCardProps {
 
 const CameraPreviewCard: React.FC<CameraPreviewCardProps> = ({ onCapture, onDisconnect }) => {
   return (
-    <Card className="max-w-md mx-auto mb-4 mt-4 absolute left-1/2 -translate-x-1/2">
-      <CardHeader className='items-center'>
+    <Card className="max-w-md mx-auto mb-4 mt-4 absolute left-1/2 -translate-x-1/2 max-h-[85vh] flex flex-col">
+      <CardHeader className='items-center flex-shrink-0'>
         <h2 className="text-lg font-semibold">Connect phone camera</h2>
-        <p className='text-sm text-center'>Stream photos from your phone to save as new positions.</p>
+        <p className='text-sm text-center'>Use your phone as a remote camera.</p>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex-1 overflow-y-auto">
         <CameraConnect
           onCapture={onCapture}
           onDisconnect={onDisconnect}
